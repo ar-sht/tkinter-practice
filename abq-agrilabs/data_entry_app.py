2f5ae566056b1875998940b20919c7a8554a6b6f
Ari Shtein <ashtein120@gmail.com>
2023-02-28 11:07:02 -0500

    Add whole form as a class

--- abq-agrilabs/data_entry_app.py
+++ abq-agrilabs/data_entry_app.py
@@ -5,6 +5,11 @@ from datetime import datetime
 from pathlib import Path
 import csv
 
+# set up root of app
+root = tk.Tk()
+root.title('ABQ Data Entry Application')
+root.columnconfigure(0, weight=1)
+
 
 class BoundText(tk.Text):
     """A Text widget with a bound variable"""
@@ -83,24 +88,190 @@ class LabelInput(tk.Frame):
         super().grid(sticky=sticky, **kwargs)
 
 
+class DataRecordForm(ttk.Frame):
+    """The input form for our widgets"""
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # holding all our data in tk vars
+        self._vars = {
+            'Date': tk.StringVar(),
+            'Time': tk.StringVar(),
+            'Technician': tk.StringVar(),
+            'Lab': tk.StringVar(),
+            'Plot': tk.IntVar(),
+            'Seed Sample': tk.StringVar(),
+            'Humidity': tk.DoubleVar(),
+            'Light': tk.DoubleVar(),
+            'Temperature': tk.DoubleVar(),
+            'Equipment Fault': tk.BooleanVar(),
+            'Plants': tk.IntVar(),
+            'Blossoms': tk.IntVar(),
+            'Fruit': tk.IntVar(),
+            'Min Height': tk.DoubleVar(),
+            'Max Height': tk.DoubleVar(),
+            'Med Height': tk.DoubleVar(),
+            'Notes': tk.StringVar()
+        }
+
+        # First section of form -  Record Info
+        r_info = self._add_frame("Record Information")
+
+        # First row of entry things for record info
+        LabelInput(
+            r_info, "Date", var=self._vars['Date']
+        ).grid(row=0, column=0)
+        LabelInput(
+            r_info, "Time", input_class=ttk.Combobox,
+            var=self._vars['Time'],
+            input_args={'values': ['8:00', '12:00', '16:00', '20:00']}
+        ).grid(row=0, column=1)
+        LabelInput(
+            r_info, "Technician", var=self._vars['Technician']
+        ).grid(row=0, column=2)
+
+        # Second row of entry things for record info
+        LabelInput(
+            r_info, "Lab", input_class=ttk.Radiobutton,
+            var=self._vars['Lab'],
+            input_args={'values': ['A', 'B', 'C']}
+        ).grid(row=1, column=0)
+        LabelInput(
+            r_info, "Plot", input_class=ttk.Combobox,
+            var=self._vars['Plot'],
+            input_args={'values': [list(range(1, 21))]}
+        ).grid(row=1, column=1)
+        LabelInput(
+            r_info, "Seed Sample", var=self._vars['Seed Sample']
+        ).grid(row=1, column=2)
+
+        # Second section of form - Environmental Data
+        e_info = self._add_frame("Environment Data")
+
+        # First row of entry things for environmental data section
+        LabelInput(
+            e_info, "Humidity (g/m³)",
+            input_class=ttk.Spinbox, var=self._vars['Humidity'],
+            input_args={'from_': 0.5, 'to': 52.0, 'increment': .01}
+        ).grid(row=0, column=0)
+        LabelInput(
+            e_info, "Light (klx)", input_class=ttk.Spinbox,
+            var=self._vars['Light'],
+            input_args={'from_': 0, 'to': 100, 'increment': .01}
+        ).grid(row=0, column=1)
+        LabelInput(
+            e_info, "Temperature (°C)",
+            input_class=ttk.Spinbox, var=self._vars['Temperature'],
+            input_args={'from_': 4, 'to': 40, 'increment': .01}
+        )
+
+        # Second row of entry thing for environmental data section
+        LabelInput(
+            e_info, "Equipment Fault",
+            input_class=ttk.Checkbutton,
+            var=self._vars['Equipment Fault']
+        ).grid(row=1, column=0, columnspan=3)
+
+        # Third section of form - Plant Data
+        p_info = self._add_frame("Plant Data")
+
+        # First row of entry things for plant data section
+        LabelInput(
+            p_info, "Plants", input_class=ttk.Spinbox,
+            var=self._vars['Plants'],
+            input_args={'from_': 0, 'to': 20}
+        ).grid(row=0, column=0)
+        LabelInput(
+            p_info, "Blossoms", input_class=ttk.Spinbox,
+            var=self._vars['Blossoms'],
+            input_args={'from_': 0, 'to': 1000}
+        ).grid(row=0, column=1)
+        LabelInput(
+            p_info, "Fruit", input_class=ttk.Spinbox,
+            var=self._vars['Fruit'],
+            input_args={'from_': 0, 'to': 1000}
+        ).grid(row=0, column=2)
+
+        # Second row of entry things for plant data section
+        LabelInput(
+            p_info, "Min Height (cm)",
+            input_class=ttk.Spinbox, var=self._vars['Min Height'],
+            input_args={'from_': 0, 'to': 1000, 'increment': .01}
+        ).grid(row=1, column=0)
+        LabelInput(
+            p_info, "Max Height (cm)",
+            input_class=ttk.Spinbox, var=self._vars['Max Height'],
+            input_args={'from_': 0, 'to': 1000, 'increment': .01}
+        ).grid(row=1, column=1)
+        LabelInput(
+            p_info, "Median Height (cm)",
+            input_class=ttk.Spinbox, var=self._vars['Median Height'],
+            input_args={'from_': 0, 'to': 1000, 'increment': .01}
+        ).grid(row=1, column=2)
+
+        # Notes section
+        LabelInput(
+            self, "Notes",
+            input_class=BoundText, var=self._vars['Notes'],
+            input_args={'width': 75, 'height': 10}
+        ).grid(sticky=tk.W, row=3, column=0)
+
+        # Buttons section
+        buttons = tk.Frame(self)
+        buttons.grid(sticky=(tk.W + tk.E), row=4)
+
+        # Save button
+        self.savebutton = ttk.Button(
+            buttons, text="Save", command=self.master._on_save
+        )
+        self.savebutton.pack(side=tk.RIGHT)
+
+        # Reset button
+        self.resetbutton = ttk.Button(
+            buttons, text="Reset", command=self.reset
+        )
+        self.resetbutton.pack(side=tk.RIGHT)
+
+    def _add_frame(self, label, cols=3):
+        """Add a LabelFrame to the form"""
+        frame = ttk.LabelFrame(self, text=label)  # create the frame with whole form as parent and label passed in
+        frame.grid(sticky=(tk.W + tk.E))  # add frame to form
+        # each column should expand evenly
+        for i in range(cols):
+            frame.columnconfigure(i, weight=1)
+        return frame
+
+    def reset(self):
+        """Resets the form entries"""
+        for var in self._vars.values():
+            if isinstance(var, tk.BooleanVar):
+                var.set(False)
+            else:
+                var.set('')
+
+    def get(self):
+        data = dict()
+        fault = self._vars['Equipment Fault'].get()
+        for key, variable in self._vars.items():
+            if fault and key in ['Light', 'Humidity', 'Temperature']:
+                data[key] = ''
+            else:
+                try:
+                    data[key] = variable.get()
+                except tk.TclError:
+                    message = f'Error in field: {key}. Data was not saved!'
+                    raise ValueError(message)
+        return data
+
+
 variables = dict()
 records_saved = 0
-
-
-root = tk.Tk()
-root.title('ABQ Data Entry Application')
-root.columnconfigure(0, weight=1)
 
 ttk.Label(
     root,
     text='ABQ Data Entry Application',
     font=('TkDefaultFont', 16)
 ).grid()
-
-
-drf = ttk.Frame(root)
-drf.grid(padx=10, sticky=(tk.W + tk.E))
-drf.columnconfigure(0, weight=1)
 
 
 r_info = ttk.LabelFrame(drf, text='Record Information')
@@ -151,7 +322,7 @@ ttk.Combobox(
     values=list(range(1, 21))
 ).grid(row=3, column=1, sticky=(tk.W + tk.E))
 
-variables['Seed Sample']= tk.StringVar()
+variables['Seed Sample'] = tk.StringVar()
 ttk.Label(r_info, text='Seed Sample').grid(row=2, column=2)
 ttk.Entry(
     r_info,
